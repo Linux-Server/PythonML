3bd802f3fb51493a2344b8f9d1333d0aab52fd44
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-12-07 21:54:33 +0530

    arithmetic operation

---
+++ artthmetic.py
@@ -0,0 +1,25 @@
+# Arithmetic Operator
+# +, - * / %
+
+# Add
+add = 1 + 1
+print(add)
+
+sub = 4 - 5
+print(sub) # ans = -1
+
+mul = 7*8  #multiplication
+
+div = 5/2
+print(div) # ans = 2.5 , division
+
+mod = 5%2
+print(mod) # ans 1 , modulus
+
+expo = 5**3
+print(expo) #ans => 5*5*5 =125 , exponentiation
+
+
+floor = 5//2
+print(floor) # 2 Floor division
+
