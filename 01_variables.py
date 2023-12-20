f05c86476acafb31bca8ece061917fd5f78d25fd
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-12-20 21:44:58 +0530

    variable and declarartion

---
+++ 01_variables.py
@@ -0,0 +1,26 @@
+#Declare a variable
+name = "sam"
+roll = 12
+status = True
+
+# Python follows naming convention either by camelcae or snake case
+
+studentArray = [1,2,3,4] # camelCase
+student_array = [1,2,3,4,5] # snake_case
+
+# Declare multiple variables 
+a=b=c=d= 100
+
+print(b)
+
+# Declare mutiple assignments
+x,y,z = 10,20,30
+print(y)
+
+# Delete a varaible
+
+age=22
+print(age)
+del age  # This way we can delete a varaible in python
+# print(age)
+
