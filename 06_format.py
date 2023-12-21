1a8ff720a4358b678922ca1a2b101050b7c42857
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-12-21 10:26:59 +0530

    formatting

---
+++ 06_format.py
@@ -0,0 +1,16 @@
+num1 = input("Enter num one : ")
+num2 = input("Enter num two : ")
+
+
+print(int(num1) + int(num2))
+
+fname ="Sachin"
+lname ="Murali"
+
+#concat in print
+
+print(fname + lname)
+
+# String replacement
+
+print("Hello {} {}".format(fname, lname))
