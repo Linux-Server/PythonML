a5dc99abcdedb69ac84412edc633a927a3a64dcd
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-12-21 10:12:49 +0530

    type casting

---
+++ 04_data_types.py
@@ -0,0 +1,43 @@
+# Numeric data types (Integer, Float, Complex)
+
+#Integer data type
+x = 10
+print(type (x))
+
+# Float data types 
+x = 10.5
+print( type(x))
+
+#complex data types which ho;d real and imaginary number
+x = 10 + 10j
+print(type(x))
+
+
+#Sequence data types (String, List ,Tuple)
+
+#String
+
+name =  'Sachin'
+name = "Anju"
+
+multi_line = "hello buddy" \
+" how are"
+
+print(multi_line)
+
+# dtring is wrapper around array , so can be accessed by index
+
+print(name[0])
+
+#to get the lenth of string
+
+print(len(multi_line))
+
+#string concat
+fName = "Sachin"
+lName = "Murali"
+
+print(fName + lName)
+
+
+
