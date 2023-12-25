520ed1b138816e6e985df4a7541f270e20f026a5
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-12-25 19:00:54 +0530

    tuple and list

---
+++ data_structures/01_built-in/tuple.py
@@ -0,0 +1,9 @@
+#Init tuple with two ways
+t1 = (1,"sam", True)
+t2 = 2,3,False
+
+#The key diffrenec is tuple is not grow or shrinable
+# t1[0] = 2
+# print(t1)
+print(t1.count("sam"))
+print(t1.index(1))
