e4c50ca049a30a77d57ae2495b7c12faf1f82b90
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2023-12-25 12:26:38 +0530

    functions

---
+++ function.py
@@ -0,0 +1,16 @@
+
+def greet(message):
+    print(message)
+
+
+greet("hello sam")   
+
+
+def calcutate_tax(num1 , num2):
+    return num1+num2
+
+
+amount = calcutate_tax(10,20) 
+
+print(amount)
+    
