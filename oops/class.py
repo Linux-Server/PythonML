bfa2b3474f9df6d1d8f0e5c6062b251b3e3b7ca7
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-02 11:43:24 +0530

    oops+ class

---
+++ oops/class.py
@@ -0,0 +1,22 @@
+class Person:
+    x =200
+    
+    def sam(self):
+        print("Hello sam")
+        return True
+    
+my_class = Person()
+
+my_class.x =500
+
+print(my_class.x)
+
+print(my_class.sam())
+
+
+my_class = Person()
+print(my_class.x)
+
+'''
+hello multiline comments
+'''
