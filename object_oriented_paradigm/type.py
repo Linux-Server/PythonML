7e82f49775252b5a411563279fe3f07e078f0e56
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-22 19:20:39 +0530

    type of inbuilt data type

---
+++ object_oriented_paradigm/type.py
@@ -0,0 +1,7 @@
+item_name = "iPhone"
+item_price =100
+item_status = True
+
+print(item_name, item_price,item_status)
+
+print(type(item_name), type(item_price), type(item_status))
---
+++ reload.py
@@ -0,0 +1,8 @@
+import os
+
+current_dir = os.listdir(r'./')
+
+curr = os.curdir
+print(curr)
+
+print(current_dir)
