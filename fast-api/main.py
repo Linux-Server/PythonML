602420a74b87e5a36e398b94efea6e2a0d36214a
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-02-10 23:53:13 +0530

    merging two dict

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
+from random import  randrange
 
 app = FastAPI()
 
@@ -8,27 +9,26 @@ app = FastAPI()
 # Schema for create a post
 class Posts(BaseModel):
     # mandatory fields
-    title:str
-    content:str
-    
-    #optional fields
-    status:bool = True
-    active:Optional[int] = None
-    
+    title: str
+    content: str
 
-@app.get("/")
-def root():
-    return {"message" : "FAst api generated"}
+    # optional fields
+    status: bool = True
+    active: Optional[int] = None
+
+
+all_posts = [{"id": 1, "title": "Post 1", "content": "Post One is here"},
+             {"id": 2, "title": "Post 2", "content": "Post Two is here"}]
 
 
 @app.get("/posts")
 def get_post():
-    return {"message": "All post fetched"}
+    return {"data": all_posts}
+
 
 @app.post("/posts")
 def create_post(payload: Posts):
     print(payload)
-    print(payload.title)
-    return {"message": "Post Created!"}
-
-
+    print(payload.dict())
+    all_posts.append({"id": randrange(3,100), **payload.dict()})
+    return {"data": payload.dict()}
