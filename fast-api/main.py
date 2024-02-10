d673dff6b762de7ca75f854880bf9b07268dee12
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-02-10 22:45:24 +0530

    fastapi with jet client

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,33 +1,34 @@
+from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
-from random import randrange
-from datetime import time
+
 app = FastAPI()
 
 
-class Post(BaseModel):
-    title: str
-    content: str
-
-
-all_post = [{"id": 1, "title": "Post 1", "content": "Hello Post 1"}]
-
+# Schema for create a post
+class Posts(BaseModel):
+    # mandatory fields
+    title:str
+    content:str
+    
+    #optional fields
+    status:bool = True
+    active:Optional[int] = None
+    
 
 @app.get("/")
 def root():
-    return {"message": "Hello World Sachin"}
-
-
-@app.post("/posts")
-def create_post(payload: Post):
-    new_post = {}   #payload.dict()
-    new_post['id'] = randrange(2,10000000)
-    new_post.update(payload)
-    print(str(time.second))
-    all_post.append(new_post)
-    return {"message": f"New Post Created with title {payload.title} and it contain : {payload.content}"}
+    return {"message" : "FAst api generated"}
 
 
 @app.get("/posts")
 def get_post():
-    return all_post
+    return {"message": "All post fetched"}
+
+@app.post("/posts")
+def create_post(payload: Posts):
+    print(payload)
+    print(payload.title)
+    return {"message": "Post Created!"}
+
+
